cf290346b8e73a0a4cb89d2ecdcdd13c99953d5f
akshayar2712 <akshayar2712@gmail.com>
2021-12-07 19:09:02 +0530

    committed solution to TDD rectangle

--- TDDrectangle/RectangleTest.java
+++ TDDrectangle/RectangleTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import java.text.DecimalFormat;
 
 public class RectangleTest {
 
@@ -30,5 +31,31 @@ public class RectangleTest {
                 area.area();}
             );
         }
+    }
+
+    @Nested
+    class TestingRectanglePerimeter {
+    @Test
+        public void checkPerimeterIsEighteenIfLengthFourAndBreadthFive(){
+            Rectangle perimeter = new Rectangle(4,5);
+            double answer = perimeter.perimeter();
+            double expected = 18;
+            Assertions.assertEquals(answer, expected);
+    }
+    @Test
+    public void checkPerimeterForDecimalMeasurements(){
+        Rectangle perimeter = new Rectangle(4.5,5.6);
+        DecimalFormat df = new DecimalFormat("#.#");
+        double answer = perimeter.perimeter();
+        double expected = 20.2;
+            Assertions.assertEquals(df.format(answer), df.format(expected));
+    }
+    @Test
+    public void throwExceptionWhenMeasurementsBecomeNegativeOrZero(){
+        Assertions.assertThrows(IllegalArgumentException.class,() -> {
+            Rectangle perimeter = new Rectangle(-2, 0);
+            perimeter.perimeter();}
+        );
+    }
     }
 }
