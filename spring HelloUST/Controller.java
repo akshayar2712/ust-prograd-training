526d6f0260b798e6f74a947130842ccc4b2b87d9
akshayar2712 <akshayar2712@gmail.com>
2021-12-21 22:02:28 -0800

    added solution to spring hello UST application

---
+++ spring HelloUST/Controller.java
@@ -0,0 +1,14 @@
+package com.example.HelloUST;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/")
+    public String hello() {
+        return "Hello UST folks. Let us start with Spring Boot";
+    }
+}
+
